9bcc08c80141a6376751106cfe0f537b728a45fb
Jonathan Gomes <jonathan.gomess1@gmail.com>
2024-05-17 15:57:30 -0300

    corrige covarege jest

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,5 @@
 provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
   region                      = "us-east-1"
-  s3_use_path_style           = false
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = false
 }
 
 data "aws_availability_zones" "available" {}
