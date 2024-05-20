5d2a1808af8f09d56b127d2fcb165092acbe2bd5
jonathansantoss <jonathan.gomess1@gmail.com>
2024-05-19 22:01:46 -0300

    Update listarproducao.usecase.ts

--- src/producao/usecases/listarproducao.usecase.ts
+++ src/producao/usecases/listarproducao.usecase.ts
@@ -10,7 +10,7 @@ class ListarProducaoUseCase {
 
   async execute(numeroPedido: number) {
     return await this.producaoRepository.findOne({
-      where: { numeroPedido },
+      where: { numeroPedido: numeroPedido },
     });
   }
 }
